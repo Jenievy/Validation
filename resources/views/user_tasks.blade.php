daf35cb7076cfa3b70a317632159706996941429
jenievy gamboa <gamboajenievy.06@gmail.com>
2024-07-30 16:04:16 +0800

    authentication

--- resources/views/user_tasks.blade.php
+++ resources/views/user_tasks.blade.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Form</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/js/bootstrap.min.js">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css">
 </head>
 <body>
 
@@ -19,6 +21,7 @@
         <div>
             <label for="status">Status:</label>
             <select name="status" id="status">
+            
             <option value="on_process">On Process</option>
             <option value="pending">Pending</option>
             <option value="completed">Completed</option>
